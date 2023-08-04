cec94c4fce18ac4e27cb484ded96f3dc2b918492
vicse <vicseore@gmail.com>
2023-08-03 22:03:36 -0500

    feat: UseState, UseEffect and CustomHook implementation

---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import PropTypes from 'prop-types';
+
+export const AddCategory = ({ onNewCategory }) => {
+
+    const [inputValue, setInputValue] = useState('');
+
+    const onInputChange = ({ target: { value } }) => {
+        setInputValue(value);
+    }
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        const input = inputValue.trim();
+        if (input.length <= 1) return;
+
+        // setCategories((cat) => [ inputValue, ...cat ]);   
+        onNewCategory(input);
+        setInputValue(''); 
+    }
+
+    return (
+        <form onSubmit={ onSubmit }>
+            <input
+                text="text"
+                placeholder="Buscar Gifs"
+                value={ inputValue }
+                onChange={ onInputChange }
+            />
+        </form>
+    )
+}
+
+AddCategory.propTypes = {
+    onNewCategory: PropTypes.func.isRequired
+}
